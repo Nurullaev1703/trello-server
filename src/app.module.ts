6cc26012bb7a137308720f4fcbbbef9d06ea4fa1
Nurullaev Ilya <nurullaev567@gmail.com>
2024-05-22 09:15:43 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ProfileModule } from './profile/profile.module';
+import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal:true}),DatabaseModule, UsersModule, ProjectsModule, AuthenticationModule, ProfileModule],
+  imports: [ConfigModule.forRoot({ isGlobal:true,
+    envFilePath: path.resolve(__dirname, '../.env')
+  }),
+    DatabaseModule, 
+    UsersModule, 
+    ProjectsModule, AuthenticationModule, ProfileModule],
   controllers: [],
   providers: [],
 })
