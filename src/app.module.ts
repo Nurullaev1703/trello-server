19d83b6f973cf48325bca8bad4a17c42b9a37556
Nurullaev Ilya <nurullaev567@gmail.com>
2024-05-22 09:25:41 +0500

    change env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,14 @@ import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ProfileModule } from './profile/profile.module';
-import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal:true,
-    envFilePath: path.resolve(__dirname, '../.env')
-  }),
+  imports: [ConfigModule.forRoot({ isGlobal:true}),
     DatabaseModule, 
     UsersModule, 
-    ProjectsModule, AuthenticationModule, ProfileModule],
+    ProjectsModule, 
+    AuthenticationModule,
+    ProfileModule],
   controllers: [],
   providers: [],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,13 +5,13 @@ import {ConfigService} from "@nestjs/config";
 @Module({
     imports:[
         TypeOrmModule.forRootAsync({
-            useFactory: () => ({
+            useFactory: (configService: ConfigService) => ({
                 type: 'mysql',
-                host: process.env.MYSQL_HOST,
-                port: parseInt(process.env.MYSQL_PORT, 10),
-                username: process.env.MYSQL_USERNAME,
-                password: process.env.MYSQL_PASSWORD,
-                database: process.env.MYSQL_DATABASE,
+                host: configService.getOrThrow("MYSQL_HOST"),
+                port: configService.getOrThrow("MYSQL_PORT"),
+                username: configService.getOrThrow("MYSQL_USERNAME"),
+                password: configService.getOrThrow("MYSQL_PASSWORD"),
+                database: configService.getOrThrow("MYSQL_DATABASE"),
                 autoLoadEntities: true,
                 synchronize: true
             }),
